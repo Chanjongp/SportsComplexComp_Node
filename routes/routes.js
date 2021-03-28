76543a90384d8525805db9a72abba98a3348efe6
Chanjongp <pcj0198@daum.net>
2021-03-28 22:21:38 +0900

    Add Seqeulize

---
+++ routes/routes.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var UserCtrl = require('../controllers/UserCtrl');
+var CompetitionCtrl = require('../controllers/CompetitionCtrl');
+var CommentCtrl = require('../controllers/CommentCtrl');
+var MeetingCtrl = require('../controllers/MeetingCtrl');
