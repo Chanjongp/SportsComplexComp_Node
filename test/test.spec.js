93c0ac1cf67c31194023df29949ba0509e1f34e5
Chanjongp <pcj0198@daum.net>
2021-04-11 18:17:37 +0900

    Add Meeting Create Test

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,6 +3,9 @@ const request = require('supertest');
 const db = require('../models');
 const should = require('should');
 
+// accounts
+var cookie;
+
 describe('회원가입, 로그인 기능은', () => {
     before(() => {
         return db.sequelize.sync({force: true})
@@ -14,7 +17,7 @@ describe('회원가입, 로그인 기능은', () => {
                 .send({email : "s94203@nate.com", password : "ckswhd123~"})
                 .expect(302)
                 .expect('Location', '/accounts/success')
-                .end(done)
+                .end(done);
         })
     })
     describe('회원가입 실패 시', () => {
@@ -33,7 +36,10 @@ describe('회원가입, 로그인 기능은', () => {
                 .post('/accounts/login')
                 .send({email : "s94203@nate.com", password : "ckswhd123~"})
                 .expect('Location', '/accounts/success')
-                .end(done)
+                .end((err, res) => {
+                    cookie = res.headers['set-cookie'];
+                    done();
+                });
         })
     })
     describe('로그인 실패 시', () => {
@@ -45,5 +51,30 @@ describe('회원가입, 로그인 기능은', () => {
                 .expect('Location', '/accounts/error')
                 .end(done);
         })
+    })
+})
+
+// Meeting
+
+describe('POST /meeting/create 는', () => {
+    const location = "서울";
+    const title = "스쿼트 같이 하실 분?";
+    const find_people = 2;
+    const body = "스쿼트 같이 할 근처에 서울사람 구합니다!";
+    const category = "스쿼트";
+    const address = "jugong3";
+    
+    describe('Meeting Create 성공 시', () => {
+       it('생성된 Meeting 객체로 반환한다.', (done) => {
+           request(app)
+                .post('/meeting/create')
+                .send({location, title, find_people, body, category, address})
+                .expect(200)
+                .set('Cookie', cookie)
+                .end((err, res) => {
+                    res.body.should.have.properties({location, title, find_people, body, category, address})
+                    done();
+                });
+       }) 
     })
 })
